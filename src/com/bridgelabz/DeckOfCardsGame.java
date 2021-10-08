41a2b3bc14f4bed03d8ce0259285fc3c6ed7a0b9
Grid112 <sumitware1999@gmail.com>
2021-10-08 22:47:00 +0530

    To Add More Than 2 & less Than 4 Players To Play The Game

--- src/com/bridgelabz/DeckOfCardsGame.java
+++ src/com/bridgelabz/DeckOfCardsGame.java
@@ -1,10 +1,16 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DeckOfCardsGame {
+    public static final Scanner Sc = new Scanner(System.in);
     public ArrayList<String> cardsDeck = new ArrayList<>();
 
+    /*
+    In this method we have initialized the size of cards.
+    In this method  we are also printing the size of card as well as all the possible combinations of cards
+     */
     public void deckOfCards(){
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
@@ -16,6 +22,21 @@ public class DeckOfCardsGame {
             }
         }
         toDisplay(cardsDeck);
+    }
+     /*
+    In this we have to define no of players should be minimum 2 and maximum 4.
+    we use if condition to chek whether entered players are between given range ie 2 to 4
+     */
+    public void noOfPlayers() {
+        System.out.print("\nEnter Between 2 to 4 Players To Platy The Game: ");
+        int player = Sc.nextInt();
+        if (player >= 2 && player <= 4) {
+            System.out.println("\n" + player + " players will play the game");
+        } else {
+            System.out.println("Please enter number of players between 2 to 4");
+            this.noOfPlayers();
+            Sc.close();
+        }
     }
     //This Method Is used to Display the Deck Of Cards
     public static void toDisplay(ArrayList<String> cardsDeck) {
--- src/com/bridgelabz/DeckOfCardsGameMain.java
+++ src/com/bridgelabz/DeckOfCardsGameMain.java
@@ -4,6 +4,7 @@ public class DeckOfCardsGameMain {
     public static void main(String[] args){
         System.out.println("Welcome To Deck Of Cards Game");
         DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
+        deckOfCardsGame.noOfPlayers();
         deckOfCardsGame.deckOfCards();
     }
 }
